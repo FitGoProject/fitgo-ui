0978b548d56e488bc461c3c77da8ce3fa9ad6e7d
Danilo Orrego <drevello@bombora.com>
2023-06-01 16:38:01 -0300

    Playing with styling

--- fitgo-ui/src/components/LoginPage.vue
+++ fitgo-ui/src/components/LoginPage.vue
@@ -14,16 +14,16 @@
         <div class="input-group">
           <button type="submit" class="btn-login">Ingresar</button>
         </div>
-        <div class="social-login">
-          <label for="social-login" class="centered-text">Inicia sesión con redes sociales</label>
+        <div class="centered-text">
+          <label for="social-login">Inicia sesión con redes sociales</label>
         </div>
         <div class="social-login">
           <img class="icon" src="@/assets/google-icon.png">
           <img class="icon" src="@/assets/twitter-icon.png">
           <img class="icon" src="@/assets/facebook-icon.png">
         </div>
-        <div class="social-login">
-          <label for="social-login" class="centered-text">¿Olvidaste tu contraseña?</label>
+        <div class="centered-text">
+          <label for="social-login">¿Olvidaste tu contraseña?</label>
         </div>
       </form>
     </div>
@@ -31,6 +31,9 @@
       <div v-for="(image, index) in campaignAssets" :key="index">
         <img v-if="currentImage === index" :src="image" class="carousel-image" alt="Campaign" />
       </div>
+    </div>
+    <div v-if="isLoading" class="overlay">
+      <div class="spinner"></div>
     </div>
   </div>
 </template>
@@ -44,6 +47,7 @@ export default {
       email: '',
       password: '',
       error: '',
+      isLoading: false,
       currentImage: 0,
       campaignAssets: [
         require('@/assets/carrousel_login_1.png'),
@@ -60,6 +64,7 @@ export default {
   },
   methods: {
     async submitForm() {
+      this.isLoading = true;
       try {
         const response = await axios.post('https://fit-go-api.onrender.com/api/users/login', {
           email: this.email,
@@ -71,6 +76,8 @@ export default {
         }
       } catch (error) {
         this.error = 'An error occurred, please try again.';
+      } finally {
+        this.isLoading = false;
       }
     },
   },
@@ -85,44 +92,57 @@ body {
   font-weight: 700;
   line-height: 39px;
   letter-spacing: 0em;
-  text-align: left;
+  text-align: center;
+  display: flex;
+  align-items: center; /* Aligns items vertically in the center */
+  justify-content: center; /* Aligns items horizontally in the center */
+  height: 100vh; /* Makes body take up full viewport height */
+  margin: 0; /* Removes default margin */
+  padding: 0; /* Removes default padding */
 }
 
 .login-container {
   display: flex;
-  width: 1170px;
-  height: 734px;
   background-color: white;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  margin: auto;
+  align-self: center;
   font-size: 15px;
-  padding: 2em;
+  max-width: 80%; /* Limits the width to 80% of the body */
+  max-height: 90%;
+  padding: 16px; /* Provides some space around the elements inside the container */
+  box-sizing: border-box; /* Makes sure padding doesn't increase the total width */
+  margin: auto; /* Centers the container on the page */
+}
+
+.login, .carousel {
+  flex: 1; /* Gives equal width to both elements */
 }
 
 .login {
   flex: 1;
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  align-items: center;
   font-family: 'Inter';
-  font-size: 12px;
   font-weight: 700;
   line-height: 39px;
   letter-spacing: 0em;
   text-align: left;
-  padding-left: 100px;
+  padding-left: 2px;
   /* Add 100px of padding to the left */
 }
 
 .centered-text {
   text-align: center;
+  width: 100%;
+  /* Make sure this class takes the full width */
+  color: #6C767E;
 }
 
 .login h1 {
-  width: 195px;
-  height: 39px;
-  display: inline-block;
-  /* change display to inline-block */
+  display: block; /* Change to block */
+  width: 100%; /* Increase the width to 100% */
+  height: auto; /* Change height to auto */
   font-family: 'Inter';
   font-style: normal;
   font-weight: 700;
@@ -130,7 +150,9 @@ body {
   line-height: 39px;
   color: #212B36;
   margin-bottom: 1em;
+  margin-top: 12px;
   text-align: left;
+  margin-left: 120px;
   /* add this line to align the h1 text to the left */
 }
 
@@ -139,20 +161,19 @@ body {
   font-family: 'Inter';
   font-size: 15px;
   flex-direction: column;
-  margin-bottom: 1em;
+  margin-bottom: 30px;
+  margin-top: 20px;
 }
 
 .input-group label {
-  margin-bottom: 0.5em;
+  margin-bottom: 0.1em;
   color: #6C767E
 }
 
 .input-group input,
 .input-group button {
   width: 400px;
-  /* set width to 400px */
   height: 48px;
-  /* set height to 48px */
   font-size: 1em;
   border: 1px solid #ddd;
   border-radius: 5px;
@@ -175,7 +196,7 @@ body {
 .social-login {
   display: flex;
   justify-content: space-between;
-  margin-top: 1em;
+  margin-top: 2px;
   text-align: center;
   font-size: 16px;
   color: #6C767E
@@ -186,6 +207,8 @@ body {
   /* set the width to 120px */
   height: 45px;
   /* set the height to 45px */
+  margin-top: 3px;
+  margin-bottom: 40px;
 }
 
 .carousel {
@@ -193,10 +216,13 @@ body {
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-top: 16px;
+  max-width: 75%;
   position: relative;
   /* Add this */
   overflow: hidden;
-  /* Add this to prevent images from extending the container */
+  border-top-right-radius: 5px;
+  border-bottom-right-radius: 5px;
 }
 
 @keyframes fadeInOut {
@@ -224,15 +250,15 @@ body {
   width: 548px;
   height: 696px;
   object-fit: contain;
+  max-height: 96%;
   opacity: 0;
+  padding-right: 2px;
   animation: fadeInOut 5s infinite;
 }
 
 .carousel-image.fade {
   animation-delay: 0s;
 }
-
-
 
 .slide-enter-active,
 .slide-leave-active {
@@ -247,7 +273,6 @@ body {
   transform: translateX(-100%);
 }
 
-
 .fade-enter-active,
 .fade-leave-active {
   transition: opacity 0.5s;
@@ -257,4 +282,33 @@ body {
 .fade-leave-to {
   opacity: 0;
 }
+
+.overlay {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 9999;
+}
+
+.spinner {
+  border: 8px solid rgba(0, 0, 0, 0.1);
+  border-left: 8px solid black;
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  animation: spin 1s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
+
 </style>
